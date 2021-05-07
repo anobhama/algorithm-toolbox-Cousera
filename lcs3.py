a16046c05542a85958579035638d41d2dd003733
anobhama <anobhama99@gmail.com>
2021-05-08 01:28:29 +0530

    week 5

---
+++ lcs3.py
@@ -0,0 +1,20 @@
+import sys
+
+def lcs3(a, b, c):
+    return min(len(a), len(b), len(c))
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    an = data[0]
+    data = data[1:]
+    a = data[:an]
+    data = data[an:]
+    bn = data[0]
+    data = data[1:]
+    b = data[:bn]
+    data = data[bn:]
+    cn = data[0]
+    data = data[1:]
+    c = data[:cn]
+    print(lcs3(a, b, c))
